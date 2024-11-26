60a123c294009899019eabf8cfb82d07d3b58844
Archange <archange_paradise@proton.me>
2024-11-26 17:26:51 +0100

    Mise à jour du code

--- Data/constants.py
+++ Data/constants.py
@@ -21,77 +21,77 @@ def load_json(path: str) -> dict:
         return json.load(file)
 
 
-ROOT_DIR: Path = Path(os.path.dirname(__file__))
-DIR_DATA: Path = ROOT_DIR / "data"
+DIR_DATA: Path = Path(os.path.dirname(__file__))
+VERSION_CAPACITIES: dict = load_json(DIR_DATA / "version_capacities.json")
 
-VERSION_CAPACITIES: dict = load_json(DIR_DATA + "version_capacities.json")
-
+# A lambda function to determinate the size of the QR-Code with the number of the version
+size = lambda version: 4 * version + 17
 
 # Constants to define the level of error correction
-L = bytes(0b00) # 7%
-M = bytes(0b01) # 15% (by default)
-Q = bytes(0b10) # 25%
-H = bytes(0b11) # 30%
+L = 0b00 # 7%
+M = 0b01 # 15% (by default)
+Q = 0b10 # 25%
+H = 0b11 # 30%
 
 # Constants to define the data type
-TYPE_1 = bytes(0b0001) # Numbers only: numeric (decimal digits from 0 to 9)
-TYPE_2 = bytes(0b0010) # Letters and numbers: alphanumeric (decimal digits from 0 to 9, 
+TYPE_1 = 0b0001 # Numbers only: numeric (decimal digits from 0 to 9)
+TYPE_2 = 0b0010 # Letters and numbers: alphanumeric (decimal digits from 0 to 9, 
                 # and upper-case letters (not lower-case!), 
                 # and the symbols $, %, *, +, -, ., /, and : as well as a space)
-TYPE_3 = bytes(0b0100) # Text coded in 8-bit values (ISO-8859-1 character set)
-TYPE_4 = bytes(0b1000) # Japanese kanji ideogram (JIS Shift pose character)
+TYPE_3 = 0b0100 # Text coded in 8-bit values (ISO-8859-1 character set)
+TYPE_4 = 0b1000 # Japanese kanji ideogram (JIS Shift pose character)
 
 CHAR_TYPE_1 = "0123456789" # all symbols authorized for type 1
 CHAR_TYPE_2 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:" # all symbols authorized for type 2
 
 # Constants to define the mask pattern
-MASK_PATTERN_0 = bytes(0b000)
-MASK_PATTERN_1 = bytes(0b001)
-MASK_PATTERN_2 = bytes(0b010)
-MASK_PATTERN_3 = bytes(0b011)
-MASK_PATTERN_4 = bytes(0b100) # by default
-MASK_PATTERN_5 = bytes(0b101)
-MASK_PATTERN_6 = bytes(0b110)
-MASK_PATTERN_7 = bytes(0b111)
+MASK_PATTERN_0 = 0b000
+MASK_PATTERN_1 = 0b001
+MASK_PATTERN_2 = 0b010
+MASK_PATTERN_3 = 0b011
+MASK_PATTERN_4 = 0b100 # by default
+MASK_PATTERN_5 = 0b101
+MASK_PATTERN_6 = 0b110
+MASK_PATTERN_7 = 0b111
 
 # Constants to define QR-Code size
-VERSION_1 = 21 # by default
-VERSION_2 = 25
-VERSION_3 = 29
-VERSION_4 = 33
-VERSION_5 = 37
-VERSION_6 = 41
-VERSION_7 = 45
-VERSION_8 = 49
-VERSION_9 = 53
-VERSION_10 = 57
-VERSION_11 = 61
-VERSION_12 = 65
-VERSION_13 = 69
-VERSION_14 = 73
-VERSION_15 = 77
-VERSION_16 = 81
-VERSION_17 = 85
-VERSION_18 = 89
-VERSION_19 = 93
-VERSION_20 = 97
-VERSION_21 = 101
-VERSION_22 = 105
-VERSION_23 = 109
-VERSION_24 = 113
-VERSION_25 = 117
-VERSION_26 = 121
-VERSION_27 = 125
-VERSION_28 = 129
-VERSION_29 = 133
-VERSION_30 = 137
-VERSION_31 = 141
-VERSION_32 = 145
-VERSION_33 = 149
-VERSION_34 = 153
-VERSION_35 = 157
-VERSION_36 = 161
-VERSION_37 = 165
-VERSION_38 = 169
-VERSION_39 = 173
-VERSION_40 = 177
+VERSION_1 = 1 # by default
+VERSION_2 = 2
+VERSION_3 = 3
+VERSION_4 = 4
+VERSION_5 = 5
+VERSION_6 = 6
+VERSION_7 = 7
+VERSION_8 = 8
+VERSION_9 = 9
+VERSION_10 = 10
+VERSION_11 = 11
+VERSION_12 = 12
+VERSION_13 = 13
+VERSION_14 = 14
+VERSION_15 = 15
+VERSION_16 = 16
+VERSION_17 = 17
+VERSION_18 = 18
+VERSION_19 = 19
+VERSION_20 = 20
+VERSION_21 = 21
+VERSION_22 = 22
+VERSION_23 = 23
+VERSION_24 = 24
+VERSION_25 = 25
+VERSION_26 = 26
+VERSION_27 = 27
+VERSION_28 = 28
+VERSION_29 = 29
+VERSION_30 = 30
+VERSION_31 = 31
+VERSION_32 = 32
+VERSION_33 = 33
+VERSION_34 = 34
+VERSION_35 = 35
+VERSION_36 = 36
+VERSION_37 = 37
+VERSION_38 = 38
+VERSION_39 = 39
+VERSION_40 = 40
--- qr_code.py
+++ qr_code.py
@@ -4,30 +4,20 @@ from kandinsky import fill_rect
 
 
 class QrCode:
-    def __init__(self, string: str, length: int = 21):
-        self.length = length
+    def __init__(self, string: str):
+        """A simple implementation of a QR-Code in a 2D matrice"""
         self.string = string
-        self.array: list[list[0]] = QrCode.zeros(length, length)
+        self.level_correction: bytes = M
+        self.version: int = self.find_best_version()
+        self.size: int = size(self.version)
+        self.array: list[list] = QrCode.fill(self.version, 0)
+
+        self.init_matrice() # to draw simple pattern in the matrice of the QR-Code
 
     @staticmethod
-    def zeros(length: int = 21, width: int = 21) -> list[list[0]]:
-        return [[0 for _ in range(length)] for _ in range(width)]
-
-    @staticmethod
-    def positioning(x, array: list[list]) -> list[list]:
-        
-
-    @staticmethod
-    def apply_positioning(array: list[list]):
-        pass
-
-    @staticmethod
-    def apply_positioning(array: list[list]):
-        pass
-
-    @staticmethod
-    def init_matrice(array: list[list]) -> list[list]:
-        pass
+    def fill(version: int = VERSION_1, fill: int = 0) -> list[list]:
+        _size = size(version)
+        return [[fill for _ in range(_size)] for _ in range(_size)]
 
     @staticmethod
     def check_data_type(string: str) -> bytes:
@@ -40,19 +30,51 @@ class QrCode:
         else:
             return TYPE_4
 
-    @staticmethod
-    def find_best_version(string: str, level_correction: bytes = M) -> bytes:
-        data_type: bytes = QrCode.check_data_type(string)
-        
+    def apply_positioning(self, x: int = 0, y: int = 0) -> list[list]:
+        for _x in range(1, 6):
+            self.array[x + _x][y] = 1
+            self.array[x + _x][y + 6] = 1
+        for _y in range(7):
+            self.array[x][y + _y] = 1
+            self.array[x + 6][y + _y] = 1
 
+        for _y in range(3):
+            for _x in range(3):
+                self.array[x + 2 + _x][y + 2 + _y] = 1
 
+    def apply_alternance_band(self, x0: int, y0: int, x1: int, y1: 0):
+        if y1 == y0:
+            for x in range(x0, x1):
+                self.array[x][y0] = 1 if not x%2 else 0
+        elif y1 > y0:
+            for y in range(y0, y1):
+                self.array[x0][y] = 1 if not y%2 else 0
+
+    def init_matrice(self):
+        length_espacement: int = size(self.version) - 16
+
+        self.apply_positioning(0, 0)
+        self.apply_positioning(9 + length_espacement, 0)
+        self.apply_positioning(0, 9 + length_espacement)
+
+        self.apply_alternance_band(8, 6, 13, 6)
+        self.apply_alternance_band(6, 8, 6, 13)
+
+        self.array[8][13] = 1
+
+        infos: list[]
+        self.apply_infos()
+
+    def find_best_version(self) -> bytes:
+        data_type: bytes = QrCode.check_data_type(self.string)
+
+        return VERSION_1
 
     def draw(self, pos_x: int = 65, pos_y: int = 20, thickness: int = 8, spacing: int = 0):
-        for x, y in [(x, y) for y in range(self.length) for x in range(self.length)]:
-            print(x, y)
-            color: tuple[int, int, int] = (0, 0, 0) if self.array[x][y] == 1 else (200, 200, 200)
+        for x, y in [(x, y) for y in range(self.size) for x in range(self.size)]:
+            color: tuple[int, int, int] = (0, 0, 0) if self.array[x][y] == 1 else "1"#(200, 200, 200)
             fill_rect(pos_x + x * (thickness + spacing), pos_y + y * (thickness + spacing), thickness, thickness, color)
 
 if __name__ == '__main__':
     qr = QrCode("")
-    #qr.draw(spacing=1)
+    qr.draw(spacing=0)
